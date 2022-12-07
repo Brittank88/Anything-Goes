ee45a842245a2c61880e3da365972999b8ef861c
Mason Hunt <masonhunt130ii@gmail.com>
2022-12-08 00:07:48 +1100

    Fix Quilt & Asset Issues

--- forge/src/main/java/com/brittank88/anything_goes/forge/AnythingGoesForge.java
+++ forge/src/main/java/com/brittank88/anything_goes/forge/AnythingGoesForge.java
@@ -4,6 +4,18 @@ import com.brittank88.anything_goes.AnythingGoes;
 import dev.architectury.platform.forge.EventBuses;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.commons.lang3.ClassLoaderUtils;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.jar.JarFile;
 
 @Mod(AnythingGoes.MOD_ID)
 public class AnythingGoesForge {
